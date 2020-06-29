f8580eb6784f6981914dcafd71c7e0d68f9746a1
Mateusz Żółtak <zozlak@zozlak.org>
2020-06-29 22:13:15 +0200

    Evaluate matches both against literal and URI values

--- src/acdhOeaw/arche/disserv/RepoResourceTrait.php
+++ src/acdhOeaw/arche/disserv/RepoResourceTrait.php
@@ -47,18 +47,20 @@ trait RepoResourceTrait {
             FROM (
                 SELECT dsid, required, count(*) AS count, sum(passed::int) AS passed
                 FROM (
-                    SELECT dsid, dspid, coalesce(required::bool, true) AS required, id IS NOT NULL OR required IS NULL AS passed
+                    SELECT dsid, dspid, coalesce(required::bool, true) AS required, coalesce(m5.id, m6.id) IS NOT NULL OR required IS NULL AS passed
                     FROM 
                         (
-                            SELECT d1.id AS dsid, r.id AS dspid, m1.value AS required, m2.value AS property, m3.value AS value
+                            SELECT d1.id AS dsid, r.id AS dspid, m1.value AS required, m2.value AS property, m3.value AS value, m4.target_id AS target_id
                             FROM
                                 (SELECT id FROM metadata WHERE property = ? AND substring(value, 1, 1000) = ?) d1
                                 LEFT JOIN relations r ON d1.id = r.target_id AND r.property = ?
                                 LEFT JOIN metadata m1 ON r.id = m1.id AND m1.property = ?
                                 LEFT JOIN metadata m2 ON r.id = m2.id AND m2.property = ?
                                 LEFT JOIN metadata m3 ON r.id = m3.id AND m3.property = ?
+                                LEFT JOIN relations m4 ON r.id = m4.id AND m4.property = 'https://vocabs.acdh.oeaw.ac.at/schema#matchesValue'
                         ) d2
-                        LEFT JOIN (SELECT id, property, value FROM metadata WHERE id = ?) m4 USING (property, value)
+                        LEFT JOIN (SELECT id, property, value FROM metadata WHERE id = ?) m5 USING (property, value)
+                        LEFT JOIN (SELECT id, property, target_id FROM relations WHERE id = ?) m6 USING (property, target_id)
                 ) d3
                 GROUP BY 1, 2
             ) d4
@@ -68,16 +70,15 @@ trait RepoResourceTrait {
             SELECT target_id AS id FROM relations WHERE id = ? AND property = ?
         ";
         $schema                         = $this->getRepo()->getSchema();
+        $resId                          = (int) substr($this->getUri(), strlen($this->getRepo()->getBaseUrl()));
         $param                          = [
-            RDF::RDF_TYPE,
-            $schema->dissService->class,
-            $schema->parent,
-            $schema->dissService->matchRequired,
-            $schema->dissService->matchProperty,
-            $schema->dissService->matchValue,
-            (int) substr($this->getUri(), strlen($this->getRepo()->getBaseUrl())),
-            (int) substr($this->getUri(), strlen($this->getRepo()->getBaseUrl())),
-            $schema->dissService->hasService,
+            RDF::RDF_TYPE, $schema->dissService->class, // d1
+            $schema->parent, // r
+            $schema->dissService->matchRequired, // m1
+            $schema->dissService->matchProperty, // m2
+            $schema->dissService->matchValue, // m3
+            $resId, $resId, // m5, m6
+            $resId, $schema->dissService->hasService, // after union
         ];
         $config                         = new SearchConfig();
         $config->metadataMode           = self::META_NEIGHBORS;
