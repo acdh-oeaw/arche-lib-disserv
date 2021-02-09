5d3da3a12a8af76a2f0add2e82c008a095c5640e
Mateusz Żółtak <zozlak@zozlak.org>
2021-02-09 15:13:46 +0100

    CI fixes (mainly PHP 8 related)

--- .github/workflows/test-resources.sh
+++ .github/workflows/test-resources.sh
@@ -3,3 +3,5 @@ echo "INSERT INTO resources (id) SELECT generate_series(10000, 10100);" | psql
 echo "INSERT INTO identifiers (ids, id) SELECT 'http://127.0.0.1/api/' || id, id FROM resources WHERE id >= 10000;" | psql
 echo "INSERT INTO identifiers (ids, id) SELECT 'https://id.acdh.oeaw.ac.at/' || id, id FROM resources WHERE id >= 10000;" | psql
 echo "INSERT INTO metadata (id, property, type, lang, value) SELECT id, 'https://vocabs.acdh.oeaw.ac.at/schema#hasFormat', 'http://www.w3.org/2001/XMLSchema#string', '', 'application/xml' FROM resources WHERE id BETWEEN 10000 AND 10020;" | psql
+echo "INSERT INTO identifiers (ids, id) VALUES ('https://vocabs.acdh.oeaw.ac.at/archeoaisets/clarin-vlo', 10100);" | psql
+echo "INSERT INTO relations (id, property, target_id) SELECT id, 'https://vocabs.acdh.oeaw.ac.at/schema#hasOaiSet', 10100 FROM resources WHERE id BETWEEN 10000 AND 10010;" | psql
